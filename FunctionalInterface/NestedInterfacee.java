03b5399e070e7783b3e191024c10aba46179bbd7
jaiswalsheetal21 <123313916+jaiswalsheetal21@users.noreply.github.com>
2024-02-22 17:41:06 +0530

    java 8

---
+++ FunctionalInterface/NestedInterfacee.java
@@ -0,0 +1,23 @@
+package FunctionalInterface;
+
+public class NestedInterfacee {
+    public interface NestedInterface {
+        public void nestedMethod();
+    }
+
+    public static void main(String[] args)
+    {
+        // Implement nested interface
+        NestedInterface nested = new NestedInterface() {
+            public void nestedMethod()
+            {
+                System.out.println(
+                        "Hello from nested interface!");
+            }
+        };
+
+        // Call nested interface method
+        nested.nestedMethod();
+    }
+}
+
---
+++ FunctionalInterface/SerializableInterface.java
@@ -0,0 +1,39 @@
+package FunctionalInterface;
+import java.io.*;
+
+class B implements Serializable
+{
+    int i;
+    String s;
+
+     
+    public B(int i,String s)
+    {
+        this.i = i;
+        this.s = s;
+    }
+}
+
+public class SerializableInterface {
+    public static void main(String[] args)
+            throws IOException, ClassNotFoundException
+    {
+        B a = new B(20,"GeeksForGeeks");
+
+        // Serializing 'a'
+        FileOutputStream fos = new FileOutputStream("xyz.txt");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(a);
+
+        // De-serializing 'a'
+        FileInputStream fis = new FileInputStream("xyz.txt");
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        B b = (B)ois.readObject();
+
+        System.out.println(b.i+" "+b.s);
+
+        // closing streams
+        oos.close();
+        ois.close();
+    }
+}
